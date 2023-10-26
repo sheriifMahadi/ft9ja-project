eb1d9827de25c505e6e49f312ea75741dfdcc336
Abdulmajid Mahadi <70480860+sheriifMahadi@users.noreply.github.com>
2023-10-26 18:06:38 +0100

    Update settings.py

--- ft9ja_project/settings.py
+++ ft9ja_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0r)%sf+1xrj+j%o9_je)7wd!o5bdhen=xwr@ar$vcoh+sy*o14'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
