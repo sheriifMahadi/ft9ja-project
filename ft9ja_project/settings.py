050ca5d82d7c36e14a64c34a68952500c62ec616
Abdulmajid Mahadi <70480860+sheriifMahadi@users.noreply.github.com>
2023-10-26 19:37:08 +0100

    Update settings.py

--- ft9ja_project/settings.py
+++ ft9ja_project/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = os.path.join(BASE_DIR, 'static/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
