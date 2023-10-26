969b6434620c23d7f02d79d863ce0c3031bfe38a
Abdulmajid Mahadi <70480860+sheriifMahadi@users.noreply.github.com>
2023-10-26 20:36:35 +0100

    Update urls.py

--- trading/urls.py
+++ trading/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="homepage")
     path("admin-view/traders", views.allTraders, name="allTraders"),
     path("admin-view/traders/<str:id>", views.getTrader, name="getTrader"),
     path("login", views.get_trader_login, name="login")
