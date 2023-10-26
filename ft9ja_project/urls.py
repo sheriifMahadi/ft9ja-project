95f71b7e2e3a7c33ac3c752dd45202aaef4a69a1
Abdulmajid Mahadi <70480860+sheriifMahadi@users.noreply.github.com>
2023-10-26 18:37:35 +0100

    Update urls.py

--- ft9ja_project/urls.py
+++ ft9ja_project/urls.py
@@ -18,6 +18,7 @@ from unicodedata import name
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
